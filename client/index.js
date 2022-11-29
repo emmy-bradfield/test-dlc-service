a2e3ca2e4243d1420b2d53f4ff5fd8ac3a4f5d6f
Emily Bradfield <emily-bradfield@outlook.com>
2022-11-29 16:57:04 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -25,15 +25,24 @@ console.log("clicked");
     gateway: false,
     learner_support: true,
     on_track: 12,
-    activities: {
+    activities: [{
       _id: "A1.1",
       href: "https://www.github.com",
+      complete: true,
+      status: "complete",
+      due: 2022-11-29,
+      submissions: 1,
+      portfolio: false
+    },
+    {
+      _id: "A1.2",
+      href: "www.fakeurl.com",
       complete: false,
       status: "in-progress",
-      due: 2022-11-29,
+      due: 2022-12-06,
       submissions: 0,
       portfolio: false
-    }
+    }]
   };
   console.log(updateTest);
   await axios.post("/learners/new-learner", updateTest).then(res => console.log(res)).catch((err) => console.log(err));
